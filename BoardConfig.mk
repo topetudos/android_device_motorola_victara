e3f6ef9cd5c16912d11cb5b16ab651f47d559ef5
linckandrea <linckandrea01@gmail.com>
2018-10-04 20:50:32 +0200

    victara: Allow additional gralloc 1.0 buffer usage bits
    * Our platform requires 0x02000000U (USAGE_PRIVATE_UNCACHED) for
      camera, video, and various other functionality
    
    (cherry-picked from https://github.com/LineageOS/android_device_samsung_msm8974-common/commit/82bf4a6bc2cffc427a3902437da7843bdec0624c)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,7 @@ TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x02000000U
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
